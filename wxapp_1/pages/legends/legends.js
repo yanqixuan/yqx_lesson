2b7fac71373e998b5ee940fb76b2b0fc4b8385d9
yanqixuan <48020563+yanqixuan@users.noreply.github.com>
2019-04-19 21:02:51 +0800

    wxapp1

---
+++ wxapp_1/pages/legends/legends.js
@@ -0,0 +1,31 @@
+Page({
+    data:{
+        legends:[
+            {
+                name:'阿卡丽',
+                title:'离群之刺',
+                src:'http://ossweb-img.qq.com/images/lol/img/champion/Akali.png'
+            },
+            {
+                name:'亚索',
+                title:'孤儿',
+                src:'http://ossweb-img.qq.com/images/lol/img/champion/Yasuo.png'
+            },
+            {
+                name:'火男',
+                title:'ap',
+                src:'http://ossweb-img.qq.com/images/lol/img/champion/Brand.png'
+            },
+            {
+                name:'盖伦',
+                title:'ADC',
+                src:'http://ossweb-img.qq.com/images/lol/img/champion/Garen.png'
+            },
+            {
+                name:'寒冰',
+                title:'T',
+                src:'http://ossweb-img.qq.com/images/lol/img/champion/Ashe.png'
+            }
+        ]
+    }
+})
